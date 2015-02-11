8b535cb278f69823931e11d82480c26aa3b110a5
Chris <chris@chrismatic.io>
2015-02-11 10:16:22 -0700

    Reformat code slightly

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ Matrix.prototype.reset = function() {
 	this.m = [1, 0, 0, 1, 0, 0];
 };
 Matrix.prototype.multiply = function(matrix) {
-	var m11 = this.m[0] * matrix.m[0] + this.m[2] * matrix.m[1];
-	var m12 = this.m[1] * matrix.m[0] + this.m[3] * matrix.m[1];
+	var m11 = this.m[0] * matrix.m[0] + this.m[2] * matrix.m[1],
+	    m12 = this.m[1] * matrix.m[0] + this.m[3] * matrix.m[1],
+	    m21 = this.m[0] * matrix.m[2] + this.m[2] * matrix.m[3],
+	    m22 = this.m[1] * matrix.m[2] + this.m[3] * matrix.m[3];
 
-	var m21 = this.m[0] * matrix.m[2] + this.m[2] * matrix.m[3];
-	var m22 = this.m[1] * matrix.m[2] + this.m[3] * matrix.m[3];
-
-	var dx = this.m[0] * matrix.m[4] + this.m[2] * matrix.m[5] + this.m[4];
-	var dy = this.m[1] * matrix.m[4] + this.m[3] * matrix.m[5] + this.m[5];
+	var dx = this.m[0] * matrix.m[4] + this.m[2] * matrix.m[5] + this.m[4],
+	    dy = this.m[1] * matrix.m[4] + this.m[3] * matrix.m[5] + this.m[5];
 
 	this.m[0] = m11;
 	this.m[1] = m12;
@@ -24,13 +23,13 @@ Matrix.prototype.multiply = function(matrix) {
 Matrix.prototype.inverse = function() {
 	var inv = new Matrix();
 	inv.m = this.m.slice(0);
-	var d = 1 / (inv.m[0] * inv.m[3] - inv.m[1] * inv.m[2]);
-	var m0 = inv.m[3] * d;
-	var m1 = -inv.m[1] * d;
-	var m2 = -inv.m[2] * d;
-	var m3 = inv.m[0] * d;
-	var m4 = d * (inv.m[2] * inv.m[5] - inv.m[3] * inv.m[4]);
-	var m5 = d * (inv.m[1] * inv.m[4] - inv.m[0] * inv.m[5]);
+	var d = 1 / (inv.m[0] * inv.m[3] - inv.m[1] * inv.m[2]),
+	    m0 = inv.m[3] * d,
+        m1 = -inv.m[1] * d,
+	    m2 = -inv.m[2] * d,
+	    m3 = inv.m[0] * d,
+	    m4 = d * (inv.m[2] * inv.m[5] - inv.m[3] * inv.m[4]),
+	    m5 = d * (inv.m[1] * inv.m[4] - inv.m[0] * inv.m[5]);
 	inv.m[0] = m0;
 	inv.m[1] = m1;
 	inv.m[2] = m2;
@@ -40,12 +39,12 @@ Matrix.prototype.inverse = function() {
 	return inv;
 };
 Matrix.prototype.rotate = function(rad) {
-	var c = Math.cos(rad);
-	var s = Math.sin(rad);
-	var m11 = this.m[0] * c + this.m[2] * s;
-	var m12 = this.m[1] * c + this.m[3] * s;
-	var m21 = this.m[0] * -s + this.m[2] * c;
-	var m22 = this.m[1] * -s + this.m[3] * c;
+	var c = Math.cos(rad),
+	    s = Math.sin(rad),
+	    m11 = this.m[0] * c + this.m[2] * s,
+	    m12 = this.m[1] * c + this.m[3] * s,
+	    m21 = this.m[0] * -s + this.m[2] * c,
+	    m22 = this.m[1] * -s + this.m[3] * c;
 	this.m[0] = m11;
 	this.m[1] = m12;
 	this.m[2] = m21;
@@ -62,17 +61,15 @@ Matrix.prototype.scale = function(sx, sy) {
 	this.m[3] *= sy;
 };
 Matrix.prototype.transformPoint = function(px, py) {
-	var x = px;
-	var y = py;
+	var x = px, y = py;
 	px = x * this.m[0] + y * this.m[2] + this.m[4];
 	py = x * this.m[1] + y * this.m[3] + this.m[5];
 	return [px, py];
 };
 Matrix.prototype.transformVector = function(px, py) {
-	var x = 0;
-	var y = 0;
-	x = px * this.m[0] + py * this.m[2];
-	y = px * this.m[1] + py * this.m[3];
-	return [x,y];
+	var x = px, y = py;
+	px = x * this.m[0] + y * this.m[2];
+	py = x * this.m[1] + y * this.m[3];
+	return [px, py];
 };
 module.exports = Matrix;
