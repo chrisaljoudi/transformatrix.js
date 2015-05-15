ffad030486a0ae04eecf5f7be9c7babd06e2b685
Gagan Bansal <gaganbansal123@gmail.com>
2015-05-15 05:07:12 -0400

    chaining pattern enabled

--- test/test.js
+++ test/test.js
@@ -28,4 +28,41 @@ describe("Matrix", function() {
 			assert.deepEqual(m.transformVector(point[0], point[1]), [point[0] * 3, point[1] * 2]);
 		});
 	});
+  describe("Create a 'blank' Matrix, execute transformation and return self", function() {
+    var m = new Matrix();
+    it("after translate, returned should be instance of Matrix", function() {
+      assert(m.translate(10,10) instanceof Matrix,'translate function returns instance of Matrix');
+    });
+    it("after scale, returned should be instance of Matrix", function() {
+      assert(m.scale(2,2) instanceof Matrix,'scale function returns instance of Matrix');
+    });
+    it("after rotate, returned should be instance of Matrix", function() {
+      assert(m.rotate(Math.PI / 180 * 45) instanceof Matrix,'rotate function returns instance of Matrix');
+    });
+    it("after inverse, returned should be instance of Matrix", function() {
+      assert(m.inverse() instanceof Matrix,'inverse function returns instance of Matrix');
+    });
+    it("after multiply, returned should be instance of Matrix", function() {
+      assert(m.multiply(new Matrix()) instanceof Matrix,'multiply function returns instance of Matrix');
+    });
+    it("after reset, returned should be instance of Matrix", function() {
+      assert(m.reset() instanceof Matrix,'reset function returns instance of Matrix');
+    });
+  });
+  describe("Create a 'blank' Matrix, execute transformation in chain", function() {
+    var m = new Matrix();
+    it("execute translate, scale, rotate in chain", function() {
+      assert.deepEqual(
+        m.translate(10,10)
+        .scale(2,2)
+        .rotate(Math.PI / 180 * 45).m,
+        [ 1.4142135623730951,
+          1.414213562373095,
+          -1.414213562373095,
+          1.4142135623730951,
+          10,
+          10 ]
+        );
+    });
+  });
 });
